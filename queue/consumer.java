b6742abacb20b3ecea659655edc0ccba8591fc6d
Phani <phani.jella@gmail.com>
2016-01-15 14:29:19 -0600

    Update consumer.java

--- queue/consumer.java
+++ queue/consumer.java
@@ -10,9 +10,9 @@ public class consumer {
 	  public static void main(String[] argv) throws Exception {
 	    ConnectionFactory factory = new ConnectionFactory();
 	    factory.setHost("localhost");
-	    factory.setPort(5672);
-	    factory.setUsername("guest");
-	    factory.setPassword("guest");
+	    //factory.setPort(5672);
+	    //factory.setUsername("guest");
+	    //factory.setPassword("guest");
 	    final Connection connection = factory.newConnection();
 	    final Channel channel = connection.createChannel();
 
