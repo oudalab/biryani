2453972108ea3ade3e8ac2dcbd75520d5af68910
phani <phani.jella@gmail.com>
2016-01-15 19:38:49 -0600

    Implemented corenlp container and ready to receive messages when continer is up

--- corenlp/consumer.java
+++ corenlp/consumer.java
@@ -1,24 +1,53 @@
+import java.io.FileReader;
 import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import com.rabbitmq.client.*;
 
 public class consumer {
 
-        private static final String TASK_QUEUE_NAME = "main_queue";
            static int count=0;
           public static void main(String[] argv) throws Exception {
              
+        	
+        	  //Read the configuration file of corenlp
+        	  String R_ip="";
+    		  int R_port=0;
+    		  String R_usr="";
+    		  String R_pass="";
+    		  String R_vhost="";
+    		  String R_queue="";
+        	  try{
+        		  FileReader reader= new FileReader("corenlp.json");
+        		  JSONObject jsonobject = (JSONObject) new JSONParser().parse(reader);
+        		  JSONObject rabbit= (JSONObject) jsonobject.get("rabbitmq");
+        		  JSONObject mongo= (JSONObject) jsonobject.get("mongodb");
+        		  
+        		  R_ip= (String) rabbit.get("ip");
+        		  R_port= Integer.parseInt((String) rabbit.get("port"));
+        		  R_usr= (String) rabbit.get("username");
+        		  R_pass= (String)rabbit.get ("password");
+        		  R_vhost= (String) rabbit.get("vhost");
+        		  R_queue=(String) rabbit.get("queue");       		  
+        	  }
+        	  catch(Exception e){
+        		  e.printStackTrace();
+        	  }
         	  
-        	  
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("rabbitmq ip address");
-            factory.setVirtualHost("/");
-            factory.setPort(port nmber);
-            factory.setUsername("username");
-            factory.setPassword("password");
+        	ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(R_ip);
+            factory.setVirtualHost(R_vhost);
+            factory.setPort(R_port);
+            factory.setUsername(R_usr);
+            factory.setPassword(R_pass);
+            
             final Connection connection = factory.newConnection();
             final Channel channel = connection.createChannel();
 
-            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+            channel.queueDeclare(R_queue, true, false, false, null);
             System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
             channel.basicQos(1);
@@ -37,7 +66,7 @@ public class consumer {
                 }
               }
             };
-            channel.basicConsume(TASK_QUEUE_NAME, false, consumer);
+            channel.basicConsume(R_queue, false, consumer);
           }
 
           private static void doWork(String task) {
