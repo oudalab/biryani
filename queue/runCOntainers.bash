65d5442f5a32d3ddef4340d57b90f863a59bca77
yanliang1102 <yanliang1102@gamil.com>
2016-01-28 17:31:46 -0600

    bash file to run containers

---
+++ queue/runCOntainers.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run Phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL1
+docker run Phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL2
+docker run Phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL3
+docker run Phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL4
+docker run Phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL5
+docker run Phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL6
+docker run Phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL7
