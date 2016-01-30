f51b8ffa08ee3bf19f118dc44d1b2c03101a011d
yanliang1102 <yanliang1102@gmail.com>
2016-01-29 20:13:52 -0600

    make a folder for test

---
+++ test/runCOntainers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL1 &
+docker run phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL2 &
+docker run phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL3 &
+docker run phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL4 &
+docker run phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL5 &
+docker run phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL6 &
+docker run phani/ccnlp:1.0 java -Xmx10g -cp ".:lib/*" consumer 96 200 yanL7
