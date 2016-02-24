87dbd142614a14da6e16b9ce303a2708bcbe0a51
phani <phani.jella@gmail.com>
2016-02-23 21:11:35 -0600

    Added code to parse remaining documents left which are not multiple of batch size(num_docs) #13 #11

--- corenlp/consumer.java
+++ corenlp/consumer.java
@@ -1,21 +1,39 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.ConnectException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
+
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.spi.LoggerFactory;
+import org.bson.Document;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
 import com.google.common.base.Stopwatch;
+import com.mongodb.Block;
+import com.mongodb.BulkUpdateRequestBuilder;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 import com.rabbitmq.client.*;
+
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
@@ -25,9 +43,12 @@ import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
+
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
@@ -44,27 +65,46 @@ public class consumer
 	private JSONParser parser = new JSONParser();
 	//need to find a way to change this 500.
 	private ArrayBlockingQueue<Annotation> queue;
+	private ArrayBlockingQueue<Envelope> env_queue;
 	private Stopwatch batch_timer;
 	private Stopwatch total_timer;
 	private Stopwatch idle_timer;
-	//private Logger log = Logger.getLogger("C://Users//Phani//workspace//Rabbitmq//src//log4j.properties");
 	private Logger log=Logger.getLogger(getClass());
 	private int docs_parsed;
+	private int docs_inserted;
+	private Channel channel=null;
+	private java.sql.Connection c=null;
 	static 
 	{
 		instance = new consumer();
 		Properties props = new Properties();
-		props.put("annotators", "tokenize, ssplit,pos,parse");
-		props.put("parse.model","edu/stanford/nlp/models/srparser/englishSR.ser.gz");
+		props.setProperty("annotators", "tokenize, ssplit,pos,parse");
+		//props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		props.setProperty("parse.model","edu/stanford/nlp/models/srparser/englishSR.ser.gz");
 		instance.pipeline = new StanfordCoreNLP(props);
 		instance.batch_timer= Stopwatch.createUnstarted();
 		instance.total_timer=Stopwatch.createStarted();
 		instance.idle_timer=Stopwatch.createStarted();
 		instance.docs_parsed=0;
-
+		/*instance.docs_inserted=0;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			instance.c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			PreparedStatement stmt = instance.c.prepareStatement("CREATE TABLE IF NOT EXISTS json_test_table (id VARCHAR PRIMARY KEY ,output VARCHAR)");
+            stmt.executeUpdate();
+		} catch ( Exception e ) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			 
+		}
+		System.out.println("Opened database successfully");
+		*/
 	}
+
 	public static void main(String[] argv) throws Exception 
 	{
+		//BasicConfigurator.configure();
+		PropertyConfigurator.configure("log4j.properties");
+
 		final int num_proc ;
 		final int num_docs;
 		final String log_token;
@@ -106,6 +146,7 @@ public class consumer
 		int M_port= 0;
 		String M_db= "";
 		instance.queue=new ArrayBlockingQueue<Annotation>(num_docs);
+		instance.env_queue= new ArrayBlockingQueue<Envelope>(num_docs);
 
 		Thread monitorThread= new Thread() {
 			public void run() {
@@ -129,7 +170,40 @@ public class consumer
 			}  
 		};
 		//start the monitor thread here.
-		monitorThread.start();
+		//monitorThread.start();
+		
+		Timer timer= new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				//System.out.println("Timer method Queue Size "+instance.queue.size());
+				int last_index= instance.env_queue.size();
+				ArrayList<Envelope>envArrayList= new ArrayList<Envelope>();
+				instance.env_queue.drainTo(envArrayList);
+				if(last_index>0)
+				{
+					try 
+					{
+						doWork(num_proc, envArrayList.size(), log_token, true);
+						instance.channel.basicAck(envArrayList.get(last_index-1).getDeliveryTag(), true);
+					} 	
+					catch (IOException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					catch(Exception e1)
+					{
+						e1.printStackTrace();
+					}
+				}
+			}
+		}, 0,60000*2);
+		//**************************************************************//
+		
+		
 
 		try 
 		{
@@ -146,7 +220,7 @@ public class consumer
 			M_ip= (String) mongo.get("ip");
 			M_port= Integer.parseInt((String) mongo.get("port"));
 			M_db= (String) mongo.get("db");
-			System.out.println(M_db); 
+			 
 		} 
 		catch (Exception e) 
 		{
@@ -161,24 +235,33 @@ public class consumer
 		factory.setPassword(R_pass);
 		try
 		{
+			
 			final Connection connection = factory.newConnection();
-			final Channel channel = connection.createChannel();
-			channel.queueDeclare(R_queue, true, false, false, null);
-			channel.basicQos(num_docs);
-			DefaultConsumer consumer_rabbimq = new DefaultConsumer(channel) 
+			instance.channel = connection.createChannel();
+			instance.channel.queueDeclare(R_queue, true, false, false, null);
+			instance.channel.basicQos(num_docs);
+			
+			DefaultConsumer consumer_rabbimq = new DefaultConsumer(instance.channel) 
 			{
 				//create this varaible to do batch acknowledgement
 				int ackCount=0;
 				@Override
 				public void handleDelivery(String consumerTag, Envelope envelope,
 						AMQP.BasicProperties properties, byte[] body) throws IOException 
-						{
+				{
 					ackCount=ackCount+1;
 					String message = new String(body, "UTF-8");
 					// System.out.println(" [x] Received  messages'");
 					try 
 					{ 
-						doWork(message,num_proc,num_docs,log_token);
+						JSONObject json = (JSONObject) instance.parser.parse(message);
+						String doc_id= (String)json.get("doc_id");
+						String article_body=(String) json.get("article_body");
+						Annotation annotation = new Annotation(article_body);
+						annotation.set(CoreAnnotations.DocIDAnnotation.class, doc_id);
+						instance.queue.put(annotation);
+						instance.env_queue.put(envelope);
+						doWork(num_proc,num_docs,log_token,false);
 					}
 					//exit error may not be a parser exception 
 					catch (Exception e) 
@@ -212,12 +295,12 @@ public class consumer
 						//ToDo: need a way to ackowledge only when the 500 docs finish parsing, make it more durable.
 						if(ackCount%num_docs==0)
 						{
-							channel.basicAck(envelope.getDeliveryTag(), true);
+							instance.channel.basicAck(envelope.getDeliveryTag(), true);
 						}
 					}
-						}
+				}
 			};
-			channel.basicConsume(R_queue, false, consumer_rabbimq);
+			instance.channel.basicConsume(R_queue, false, consumer_rabbimq);
 		}catch(ConnectException e2)
 		{
 			e2.printStackTrace();
@@ -228,17 +311,13 @@ public class consumer
 
 		}
 	}
-	private static void doWork(String input,int num_proc,int num_docs,String log_token) throws Exception {
-		JSONObject json = (JSONObject) instance.parser.parse(input);
-		String doc_id= (String)json.get("doc_id");
-		String article_body=(String) json.get("article_body");
-		Annotation annotation = new Annotation(article_body);
-		annotation.set(CoreAnnotations.DocIDAnnotation.class, doc_id);
-		instance.queue.put(annotation);
-		//set this to check if it is 0;
-		//
-		if(instance.queue.remainingCapacity()==0 || instance.idle_timer.elapsed(TimeUnit.MILLISECONDS)>=60000)
+	private static void doWork(int num_proc,int num_docs,String log_token, boolean flush) throws Exception {
+		//System.out.println("Inside dowork queue size"+instance.queue.size());
+		if(instance.queue.remainingCapacity()==0 || flush==true)
 		{
+			if(flush)
+				instance.log.debug(log_token+"Timeout Parsing");
+			instance.env_queue.clear();
 			String id= UUID.randomUUID().toString();
 			instance.log.debug(log_token+": "+id+" Standord Thread started");
 			instance.batch_timer.start();
@@ -252,6 +331,9 @@ public class consumer
 					{
 						instance.docs_parsed++;
 						String doc_id= arg0.get(CoreAnnotations.DocIDAnnotation.class);
+						JSONObject doc_out= new JSONObject(); // main object
+						doc_out.put("doc_id", doc_id);
+						JSONObject sen_obj= new JSONObject(); // sentence object;
 						instance.log.debug(doc_id+": PARSING");
 						List<CoreMap> sentences = arg0.get(SentencesAnnotation.class);
 						for(CoreMap sentence: sentences) 
@@ -263,22 +345,38 @@ public class consumer
 								String ne = token.get(NamedEntityTagAnnotation.class);
 							}*/
 							Tree tree = sentence.get(TreeAnnotation.class);
+							sen_obj.put(sentence, tree);
 							//SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
 						}
 						instance.log.debug(doc_id+": PARSED");
+						doc_out.put("sentences", sen_obj);
+						/*try {
+							PreparedStatement stmt = instance.c.prepareStatement("INSERT INTO json_test_table (id, output) VALUES (?,?)");
+							stmt.setString(1, doc_id.toString());
+							stmt.setString(2, doc_out.toJSONString());
+							if(stmt.executeUpdate()==1){
+								System.out.println(++instance.docs_inserted+": Successfully inserted");
+							}
+							 
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}*/
 					}
 				});
 
 			}
-			instance.log.debug(log_token+" #Documents: "+instance.docs_parsed);
-			instance.log.debug(log_token+" Batch time: "+instance.batch_timer);
+			//instance.log.debug(log_token+" #Documents: "+instance.docs_parsed);
+			//String Stanford_timing=instance.pipeline.timingInformation();
+			//instance.log.debug("Stanford Timing: "+Stanford_timing);
+			instance.log.debug(log_token+" #Batch_Doc:" +num_docs +" Batch time: "+instance.batch_timer);
 			instance.batch_timer.reset();
-			instance.log.debug(log_token+" Total time: "+instance.total_timer);
+			instance.log.debug(log_token+" #Documents:" +instance.docs_parsed +" Total time: "+instance.total_timer);
 			instance.idle_timer.reset();
 			instance.idle_timer.start();
 			tempArraylist.clear();	 
-			StanfordCoreNLP.clearAnnotatorPool();
 			instance.log.debug(log_token+": "+id+" Stanford Thread completed");
 		}
 	}
 }
+
