984329043259ee00c86710ff5f6d6253521317b1
Phanindra <phani.jella@gmail.com>
2016-03-26 02:02:33 -0500

    Getting events and verbs from petrarch2 output #7

--- test/petrarch_output_from_sqlitedb.py
+++ test/petrarch_output_from_sqlitedb.py
@@ -11,10 +11,23 @@ for row in rows:
 	doc_id= row[0].encode()
 	data=row[1]
 	mongo_id=row[2]
+	num_sents=row[3]
 	data= json.loads(data)
 	data_array=json.loads(data['main_output'])
-	print 'Getting data of: '+doc_id	
+	#Code for events
 	for element in data_array:
-		element_json= json.loads(element)
-		print element_json[mongo_id]['meta']
-		
+		element_json=json.loads(element)
+		if 'sents' in element_json[mongo_id]:
+			for i in range(1,num_sents+1):
+				if element_json[mongo_id]['sents']!=None:
+					if str(i) in element_json[mongo_id]['sents']:
+						if 'events' in element_json[mongo_id]['sents'][str(i)]:
+							print 'mongo_id: '+mongo_id
+							print 'sen_id: '+str(i)
+							print 'sentence:'+element_json[mongo_id]['sents'][str(i)]['content']
+							print element_json[mongo_id]['sents'][str(i)]['events']
+				#code for getting verbs	
+				#if 'verbs' in element_json[mongo_id]['meta']:
+				#	print element_json[mongo_id]['meta']['verbs']
+
+			
