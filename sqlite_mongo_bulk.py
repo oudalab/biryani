bda557c8a03a63ec6ea52c89a434dc715b787931
Phanindra <phani.jella@gmail.com>
2016-03-29 13:22:59 -0500

    Added timing information and also using mongo_id as reference for updating

--- sqlite_mongo_bulk.py
+++ sqlite_mongo_bulk.py
@@ -1,19 +1,23 @@
+import datetime
 import sqlite3
 from bson import json_util
+from bson.objectid import ObjectId
 import json
 from pymongo import MongoClient
+#code for timing
+start_time=datetime.datetime.now()
 # Code for Mongodb
 client = MongoClient()
 db = client.database
 stories=db.collection
 bulk = stories.initialize_unordered_bulk_op()
 #Code for SQlite
-conn = sqlite3.connect('database.db')
+conn = sqlite3.connect('test.db')
 c = conn.cursor()
-c.execute("query")
+c.execute("select * from json_test_table")
 rows= c.fetchall()
 for row in rows:
-        doc_id= row[0]
+        mongo_id= row[3]
         #data_json=json.loads(row[1])
         #for sentence in data_json['sentences']:
                 #sen_dump=json.dumps(sentence)
@@ -22,7 +26,9 @@ for row in rows:
                 #sen_data=sen_json['sentence']
                 #print sen_id
                 #print sen_data
-        sentences=row[1]
-        bulk.find({'doc_id': doc_id}).update({'$set': {'parsed_data': sentences}})
+        sentences=row[2]
+        bulk.find({'_id': ObjectId(mongo_id)}).update({'$set': {'corenlp_output': sentences}})
 result= bulk.execute()
-
+end_time=datetime.datetime.now()
+time_taken=start_time-end_time
+print time_taken.total_seconds()
