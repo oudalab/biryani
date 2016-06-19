10dcd23246784e7d4f967ceb7f683cf1571d6ed6
Phani <phani.jella@gmail.com>
2016-06-19 12:20:44 -0500

    Now recording number of I/O changes

--- corenlp/stats.java
+++ corenlp/stats.java
@@ -12,7 +12,7 @@ public class stats
 		try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:"+db_name+".db");
-            PreparedStatement stmt = c.prepareStatement("CREATE TABLE IF NOT EXISTS stats_table (threads VARCHAR , batchSize VARCHAR, docsSize VARCHAR,sentences VARCHAR,timeTaken VARCHAR)");
+            PreparedStatement stmt = c.prepareStatement("CREATE TABLE IF NOT EXISTS stats_table (threads VARCHAR , batchSize VARCHAR, docsSize VARCHAR,sentences VARCHAR,io_operations VARCHAR,timeTaken VARCHAR)");
             stmt.executeUpdate();
              
         } catch ( Exception e ) {
@@ -21,19 +21,20 @@ public class stats
 
         }
 	}
-	public boolean insert_data(String db_name,String threads, String batchSize,String docSize,String sentences,String timeTaken )
+	public boolean insert_data(String db_name,String threads, String batchSize,String docSize,String sentences,String io_operations,String timeTaken )
 	{
 		try 
 		{
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:"+db_name+".db");
 	        PreparedStatement P_stmt;
-	        P_stmt= c.prepareStatement("INSERT INTO stats_table (threads,batchSize,docsSize,sentences,timeTaken) VALUES (?,?,?,?,?)");
+	        P_stmt= c.prepareStatement("INSERT INTO stats_table (threads,batchSize,docsSize,sentences,io_operations,timeTaken) VALUES (?,?,?,?,?,?)");
 	        P_stmt.setString(1, threads);
 	        P_stmt.setString(2, batchSize);
 	        P_stmt.setString(3, docSize);
 	        P_stmt.setString(4, sentences);
-	        P_stmt.setString(5,timeTaken);
+	        P_stmt.setString(5, io_operations);
+	        P_stmt.setString(6,timeTaken);
 	        if(P_stmt.execute())
 	        {
 	            return true;
