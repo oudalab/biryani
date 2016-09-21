3c0391c91f881aa4da3717104b82bd8479dff4fe
Phani <phani.jella@gmail.com>
2016-09-20 22:12:22 -0500

    Update Arabic_stories_to_xml.py

--- utilities/Arabic_stories_to_xml.py
+++ utilities/Arabic_stories_to_xml.py
@@ -1,18 +1,21 @@
+# -*- coding: utf-8 -*-
 import pymongo
 from pymongo import MongoClient
-from lxml import etree
+import xml.etree.ElementTree as etree
 
 client = MongoClient()
 db = client.lexisnexis
 stories = db.gigaword
-page = etree.Element('Sentences')
-doc = etree.ElementTree(page)
+root = etree.Element('Articles')
 doc_count=0;
-for story in stories.find({"language" : "ARB"}).limit(1):
-	sentenceElt=etree.SubElement(page, 'Sentence', date=story['publication_date'].strftime('%Y%m%d') , id=story['doc_id'], mongoId=str(story['_id']), source=story['news_source'], sentence="true")
-	title = etree.SubElement(sentenceElt, 'Text')
-	title.text = story['article_body']
-	doc_count+=1
-	print doc_count
-	outFile = open('Arabic.xml', 'w')
-doc.write(outFile)
+for story in stories.find({"language" : "ARB"}).limit(500):
+        sentenceElt=etree.SubElement(root, 'Article', date=story['publication_date'].strftime('%Y%m%d') , id=story['doc_id'], mongoId=str(story['_id']), source=story['news_source'], sentence="true")
+        title = etree.SubElement(sentenceElt, 'Text')
+        title.text = story['article_body']
+        #print story['article_body']
+        #exit(1)
+        doc_count+=1
+        print doc_count
+
+tree = etree.ElementTree(root)
+tree.write('sample2.xml','UTF-8')
