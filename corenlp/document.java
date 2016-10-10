6f090630ecadd64ad66c4d47f25c1a94a3d6b801
Phanindra <phani.jella@gmail.com>
2016-10-10 15:13:03 -0500

    use new rabbitmq API #31

---
+++ corenlp/document.java
@@ -0,0 +1,69 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class document 
+{
+	private String doc_id;
+	private String article_body;
+	private String pubclication_date;
+	private String mongo_id;
+	
+	public String getDoc_id() 
+	{
+		return doc_id;
+	}
+	public void setDoc_id(String doc_id) 
+	{
+		this.doc_id = doc_id;
+	}
+	public String getArticle_body() 
+	{
+		return article_body;
+	}
+	public void setArticle_body(String article_body) 
+	{
+		this.article_body = article_body;
+	}
+	public String getPubclication_date() 
+	{
+		return pubclication_date;
+	}
+	public void setPubclication_date(String pubclication_date) 
+	{
+		this.pubclication_date = pubclication_date;
+	}
+	public String getMongo_id() 
+	{
+		return mongo_id;
+	}
+	public void setMongo_id(String mongo_id) 
+	{
+		this.mongo_id = mongo_id;
+	}
+	
+	document(String json_document)
+	{
+		try 
+		{
+			JSONParser json_parser = new JSONParser();
+			JSONObject json = (JSONObject) json_parser.parse(json_document);
+			String doc_id= (String)json.get("doc_id");
+            String article_body=(String) json.get("article_body");
+            String pub_date=(String) json.get("publication_date_raw");
+            String mongo_id_json_str=json.get("_id").toString();
+            JSONObject mongo_id_json_obj= (JSONObject) json_parser.parse(mongo_id_json_str);
+            String mongo_id= (String) mongo_id_json_obj.get("$oid");
+            
+            setDoc_id(doc_id);
+            setArticle_body(article_body);
+            setPubclication_date(pub_date);
+            setMongo_id(mongo_id);
+		} 
+		catch (ParseException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
